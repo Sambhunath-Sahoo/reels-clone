acbac8b7a6e79cbe9873a0951a391f9b6797130d
Sambhunath-Sahoo <sambhunathsahoo25@gmail.com>
2021-12-08 10:08:07 +0530

    code formatting

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,78 +1,122 @@
-import * as React from 'react';
-import './Signup.css'
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { makeStyles } from '@mui/styles';
-import Alert from '@mui/material/Alert';
-import TextField from '@mui/material/TextField';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import "./Signup.css";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { makeStyles } from "@mui/styles";
+import Alert from "@mui/material/Alert";
+import TextField from "@mui/material/TextField";
+import { Link } from "react-router-dom";
 
-import insta from '../Assets/Instagram.JPG'
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import insta from "../Assets/Instagram.JPG";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
 export default function Login() {
-
     // appled manual style for card
     const useStyles = makeStyles({
         text1: {
-            color: 'gray',
-            textAlign: 'center'
+            color: "gray",
+            textAlign: "center",
         },
-        card2:{
-            height:'3vh',
-            marginTop:'2%'
-        }
-    })
+        card2: {
+            height: "3vh",
+            marginTop: "2%",
+        },
+    });
     const classes = useStyles();
-
 
     return (
         <div className="signup__wrapper">
             <div className="signup__card">
-                <Card variant='outlined'>
-
+                <Card variant="outlined">
                     <div className="signup__logo">
                         <img src={insta} alt="" />
                     </div>
 
                     <CardContent>
-
-                        <Typography className={classes.text1} variant="subtitile1" component="div">
+                        <Typography
+                            className={classes.text1}
+                            variant="subtitile1"
+                            component="div"
+                        >
                             Sign up to see photos and videos from your friends
                         </Typography>
 
-                        {true && <Alert severity="error">This is an error alert — check it out!</Alert>}
+                        {true && (
+                            <Alert severity="error">
+                                This is an error alert — check it out!
+                            </Alert>
+                        )}
 
-                        <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth={true} margin='dense' size='small' />
-                        <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth={true} margin='dense' size='small' />
-                        <TextField id="outlined-basic" label="Full Name" variant="outlined" fullWidth={true} margin='dense' size='small' />
-                        <Button size="small" color='secondary' fullWidth={true} variant='outlined' margin='dense' startIcon={<CloudUploadIcon />} component='label'>
-                            Upload Profile Image <input type='file' accept='image/*' hidden />
+                        <TextField
+                            id="outlined-basic"
+                            label="Email"
+                            variant="outlined"
+                            fullWidth={true}
+                            margin="dense"
+                            size="small"
+                        />
+                        <TextField
+                            id="outlined-basic"
+                            label="Password"
+                            variant="outlined"
+                            fullWidth={true}
+                            margin="dense"
+                            size="small"
+                        />
+                        <TextField
+                            id="outlined-basic"
+                            label="Full Name"
+                            variant="outlined"
+                            fullWidth={true}
+                            margin="dense"
+                            size="small"
+                        />
+                        <Button
+                            size="small"
+                            color="secondary"
+                            fullWidth={true}
+                            variant="outlined"
+                            margin="dense"
+                            startIcon={<CloudUploadIcon />}
+                            component="label"
+                        >
+                            Upload Profile Image <input type="file" accept="image/*" hidden />
                         </Button>
-
                     </CardContent>
 
                     <CardActions>
-                        <Button color='primary' fullWidth={true} variant='contained'>SIGN UP</Button>
+                        <Button color="primary" fullWidth={true} variant="contained">
+                            SIGN UP
+                        </Button>
                     </CardActions>
 
                     <CardContent>
-                        <Typography className={classes.text1} variant="subtitile1" component="div">
-                            By signing up, you agree to our Terms , Data Policy and Cookies Policy
+                        <Typography
+                            className={classes.text1}
+                            variant="subtitile1"
+                            component="div"
+                        >
+                            By signing up, you agree to our Terms , Data Policy and Cookies
+                            Policy
                         </Typography>
                     </CardContent>
-
                 </Card>
                 <Card className={classes.card2}>
-                    <Typography className={classes.text1} variant="subtitile1" component="div">
-                        Having an account? <Link to='/login' style={{textDecoration: 'none'}}>Login</Link>
+                    <Typography
+                        className={classes.text1}
+                        variant="subtitile1"
+                        component="div"
+                    >
+                        Having an account?{" "}
+                        <Link to="/login" style={{ textDecoration: "none" }}>
+                            Login
+                        </Link>
                     </Typography>
                 </Card>
             </div>
         </div>
-
     );
 }
