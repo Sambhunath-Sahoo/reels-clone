583ab406704cc9a31a587db42fe51b09ad4c58c8
Sambhunath-Sahoo <sambhunathsahoo25@gmail.com>
2021-12-13 09:13:53 +0530

    wrapped with authProvider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import Feed from "./components/Feed";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-      </Switch>
+      <AuthProvider >
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <PrivateRoute path="/" component={Feed} />
+        </Switch>
+      </AuthProvider>
     </Router>
   );
 }
